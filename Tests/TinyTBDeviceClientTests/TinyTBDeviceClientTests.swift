c6bbde247641700e6b8d80170d1e6d9c3a2f4f9e
Johannes Kinzig <mail@johanneskinzig.com>
2026-02-14 21:20:12 +0100

    Added test cases for client init and connect

--- Tests/TinyTBDeviceClientTests/TinyTBDeviceClientTests.swift
+++ Tests/TinyTBDeviceClientTests/TinyTBDeviceClientTests.swift
@@ -1,6 +1,60 @@
+import NIO
+import Foundation
+import Logging
+
 import Testing
 @testable import TinyTBDeviceClient
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+
+class ConnectivityTest {
+
+    var tinyClient: TinyTBDeviceClient? = nil
+    let eventLoopGroup: EventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+    let logger = Logger(label: "TinyTBDeviceClient")
+
+    func setupClient() throws {
+        guard let clientCredentials = ConfigLoader(searchPath: "Credentials").loadClientCredentialsFromFile(fileName: "credentials.json") else {
+            fatalError("Unable to read client credentials!")
+        }
+
+        do {
+            self.tinyClient = try TinyTBDeviceClient(
+                host: clientCredentials.host,
+                port: clientCredentials.port,
+                clientId: clientCredentials.clientId,
+                caCertPath: clientCredentials.caCertPath,
+                username: clientCredentials.username,
+                password: clientCredentials.password,
+                eventLoopGroupProvider: .shared(eventLoopGroup),
+                logger: logger
+            )
+        } catch {
+            fatalError("Unable to initialize client: \(error)")
+        }
+        try #require(self.tinyClient != nil)
+    }
+
+    func connect() async throws -> Bool {
+        var connectionSuccess = false
+        self.tinyClient?.connect(
+            onSuccess: {
+                connectionSuccess = true
+            },
+            onError: { connectionError in
+                Issue.record("Failed to connect: \(connectionError)")
+            }
+        )
+        try await Task.sleep(for: .seconds(3))
+        try #require(connectionSuccess)
+        return connectionSuccess
+    }
+
+    @Test("Client can connect")
+    func canConnect() async throws {
+        try self.setupClient()
+        let success = try await connect()
+        self.tinyClient?.disconnect()
+        //if !success { try await eventLoopGroup.shutdownGracefully() }
+        #expect(success, "Should connect successfully")
+    }
 }
