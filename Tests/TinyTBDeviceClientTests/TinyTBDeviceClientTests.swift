144a291e6269c5bce185390c06407d422c40d260
Johannes Kinzig <mail@johanneskinzig.com>
2026-02-15 14:34:46 +0100

    Added testable class and test cases for connect and disconnect (integration test, no unit tests based on injected mocks)

--- Tests/TinyTBDeviceClientTests/TinyTBDeviceClientTests.swift
+++ Tests/TinyTBDeviceClientTests/TinyTBDeviceClientTests.swift
@@ -5,14 +5,21 @@ import Logging
 import Testing
 @testable import TinyTBDeviceClient
 
-
-class ConnectivityTest {
+/// TinyTBDeviceClient Integration tests
+///
+/// Simple connectivity integration tests to check if basic functionality is working.
+/// For a detailed and sample implementation, refer to https://github.com/JoooHannesk/TinyTBDeviceClient-Example
+class ConnectivityIntegrationCUT {
 
     var tinyClient: TinyTBDeviceClient? = nil
     let eventLoopGroup: EventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
     let logger = Logger(label: "TinyTBDeviceClient")
 
-    func setupClient() throws {
+    deinit {
+        self.tinyClient = nil
+    }
+
+    init() throws {
         guard let clientCredentials = ConfigLoader(searchPath: "Credentials").loadClientCredentialsFromFile(fileName: "credentials.json") else {
             fatalError("Unable to read client credentials!")
         }
@@ -49,12 +56,51 @@ class ConnectivityTest {
         return connectionSuccess
     }
 
-    @Test("Client can connect")
-    func canConnect() async throws {
-        try self.setupClient()
-        let success = try await connect()
-        self.tinyClient?.disconnect()
-        //if !success { try await eventLoopGroup.shutdownGracefully() }
-        #expect(success, "Should connect successfully")
+    func disconnect() async throws -> Bool {
+        var disconnectSuccess = false
+        self.tinyClient?.disconnect(
+            onSuccess: {
+                disconnectSuccess = true
+            },
+            onError: { disconnectError in
+                Issue.record("Failed to connect: \(disconnectError)")
+            })
+        try await Task.sleep(for: .seconds(3))
+        try #require(disconnectSuccess)
+        return disconnectSuccess
     }
+
+    func subscribe() async throws -> Bool {
+        var subscriptionSuccess = false
+        self.tinyClient?.subscribe(
+            to: ["v1/devices/me/rpc/request/+"],
+            onSuccess: { topic, subAck in
+                print("✅ Subscribed to \(topic) with \(subAck)")
+                subscriptionSuccess = true
+            },
+            onError: { subscriptionError in
+                Issue.record("Failed to subscribe: \(subscriptionError)")
+            })
+        try await Task.sleep(for: .seconds(3))
+        try #require(subscriptionSuccess)
+        return subscriptionSuccess
+    }
+}
+
+@Test("Client can connect")
+func canConnect() async throws {
+    let clientOUT = try ConnectivityIntegrationCUT()
+    let conSuccess = try await clientOUT.connect()
+    let disconSuccess = try await clientOUT.disconnect()
+    #expect(conSuccess, "Should connect successfully")
+    #expect(disconSuccess, "Should disconnect successfully")
+}
+
+@Test("Client can subscribe")
+func canSubscribe() async throws {
+    let clientOUT = try ConnectivityIntegrationCUT()
+    let _ = try await clientOUT.connect()
+    let subSuccess = try await clientOUT.subscribe()
+    let _ = try await clientOUT.disconnect()
+    #expect(subSuccess, "Should subscribe successfully")
 }
