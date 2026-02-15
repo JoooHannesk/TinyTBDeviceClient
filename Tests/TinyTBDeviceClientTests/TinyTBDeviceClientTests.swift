c2cccb8a88d87ff9a9226d854725e9e2aa28afc0
Johannes Kinzig <mail@johanneskinzig.com>
2026-02-15 17:10:30 +0100

    Improved tests

--- Tests/TinyTBDeviceClientTests/TinyTBDeviceClientTests.swift
+++ Tests/TinyTBDeviceClientTests/TinyTBDeviceClientTests.swift
@@ -5,25 +5,25 @@ import Logging
 import Testing
 @testable import TinyTBDeviceClient
 
+
 /// TinyTBDeviceClient Integration tests
 ///
 /// Simple connectivity integration tests to check if basic functionality is working.
 /// For a detailed and sample implementation, refer to https://github.com/JoooHannesk/TinyTBDeviceClient-Example
 class ConnectivityIntegrationCUT {
 
+    var testTimeout: Duration = .seconds(1)
     var tinyClient: TinyTBDeviceClient? = nil
     let eventLoopGroup: EventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
     let logger = Logger(label: "TinyTBDeviceClient")
+    static let clientCredentials: MQTTClientCredentials = ConfigLoader(searchPath: "Credentials").loadClientCredentialsFromFile(fileName: "credentials.json")!
+
 
     deinit {
         self.tinyClient = nil
     }
 
-    init() throws {
-        guard let clientCredentials = ConfigLoader(searchPath: "Credentials").loadClientCredentialsFromFile(fileName: "credentials.json") else {
-            fatalError("Unable to read client credentials!")
-        }
-
+    init(clientCredentials: MQTTClientCredentials = ConnectivityIntegrationCUT.clientCredentials) throws {
         do {
             self.tinyClient = try TinyTBDeviceClient(
                 host: clientCredentials.host,
@@ -41,37 +41,37 @@ class ConnectivityIntegrationCUT {
         try #require(self.tinyClient != nil)
     }
 
-    func connect() async throws -> Bool {
-        var connectionSuccess = false
+    func connect() async throws -> Bool? {
+        var connectionSuccess: Bool? = nil
         self.tinyClient?.connect(
             onSuccess: {
                 connectionSuccess = true
             },
             onError: { connectionError in
-                Issue.record("Failed to connect: \(connectionError)")
+                connectionSuccess = false
             }
         )
-        try await Task.sleep(for: .seconds(3))
-        try #require(connectionSuccess)
+        try await Task.sleep(for: testTimeout)
+        try #require(connectionSuccess != nil)
         return connectionSuccess
     }
 
-    func disconnect() async throws -> Bool {
-        var disconnectSuccess = false
+    func disconnect() async throws -> Bool? {
+        var disconnectSuccess: Bool? = nil
         self.tinyClient?.disconnect(
             onSuccess: {
                 disconnectSuccess = true
             },
             onError: { disconnectError in
-                Issue.record("Failed to connect: \(disconnectError)")
+                disconnectSuccess = false
             })
-        try await Task.sleep(for: .seconds(3))
-        try #require(disconnectSuccess)
+        try await Task.sleep(for: testTimeout)
+        try #require(disconnectSuccess != nil)
         return disconnectSuccess
     }
 
-    func subscribe() async throws -> Bool {
-        var subscriptionSuccess = false
+    func subscribe() async throws -> Bool? {
+        var subscriptionSuccess: Bool? = nil
         self.tinyClient?.subscribe(
             to: ["v1/devices/me/rpc/request/+"],
             onSuccess: { topic, subAck in
@@ -79,28 +79,61 @@ class ConnectivityIntegrationCUT {
                 subscriptionSuccess = true
             },
             onError: { subscriptionError in
-                Issue.record("Failed to subscribe: \(subscriptionError)")
+                subscriptionSuccess = false
             })
-        try await Task.sleep(for: .seconds(3))
-        try #require(subscriptionSuccess)
+        try await Task.sleep(for: testTimeout)
+        try #require(subscriptionSuccess != nil)
         return subscriptionSuccess
+    }
+
+    func publish() async throws -> Bool? {
+        var publishSuccess: Bool? = nil
+        self.tinyClient?.publish(
+            message: #"{ "testMessage": "Hello ThingsBoard at \#(Date())"}"#,
+            to: "v1/devices/me/telemetry",
+            onSuccess: {
+                print("✅ Published message")
+                publishSuccess = true
+            },
+            onError: { publishError in
+                publishSuccess = false
+            })
+            try await Task.sleep(for: testTimeout)
+            try #require(publishSuccess != nil)
+            return publishSuccess
     }
 }
 
-@Test("Client can connect")
+@Test("Client-Connects")
 func canConnect() async throws {
     let clientOUT = try ConnectivityIntegrationCUT()
     let conSuccess = try await clientOUT.connect()
     let disconSuccess = try await clientOUT.disconnect()
-    #expect(conSuccess, "Should connect successfully")
-    #expect(disconSuccess, "Should disconnect successfully")
+    #expect(conSuccess!, "Should connect successfully")
+    #expect(disconSuccess!, "Should disconnect successfully")
 }
 
-@Test("Client can subscribe")
+@Test("Client-Subscribes")
 func canSubscribe() async throws {
     let clientOUT = try ConnectivityIntegrationCUT()
     let _ = try await clientOUT.connect()
     let subSuccess = try await clientOUT.subscribe()
     let _ = try await clientOUT.disconnect()
-    #expect(subSuccess, "Should subscribe successfully")
+    #expect(subSuccess!, "Should subscribe successfully")
+}
+
+@Test("Client-Cannot-Subscribe")
+func cannotSubscribe() async throws {
+    let clientOUT = try ConnectivityIntegrationCUT()
+    let subSuccess = try await clientOUT.subscribe()
+    #expect(!subSuccess!, "Should not subscribe successfully")
+}
+
+@Test("Client-Publishes")
+func canPublish() async throws {
+    let clientOUT = try ConnectivityIntegrationCUT()
+    let _ = try await clientOUT.connect()
+    let pubSuccess = try await clientOUT.publish()
+    let _ = try await clientOUT.disconnect()
+    #expect(pubSuccess!, "Should publish successfully")
 }
